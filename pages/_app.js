93645071e50f1ef3f6019d0ec15420331e47f87a
ChristosPetrou <c.petrou@tiomarkets.com>
2023-04-19 13:31:47 +0300

    retry deployment

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import PageLayout from "components/PageLayout";
 import "../styles/global.css";
 import "../styles/nprogress.css";
-import "highlight.js/styles/base16/Helios.css";
 
 import NProgress from "nprogress";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import ThemeProvider from "providers/ThemeProvider";
+import "highlight.js/styles/base16/helios.css";
 
 export default function App({ Component, pageProps }) {
   const { events } = useRouter();
